7f72207d7a7e048718fc76045a4c56090ba4dd62
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-05-04 21:48:43 +0200

    Implemented logic for parsing YAML files

--- src/main/java/com/drift/detector/YamlLoader.java
+++ src/main/java/com/drift/detector/YamlLoader.java
@@ -1,4 +1,17 @@
 package com.drift.detector;
 
+import org.yaml.snakeyaml.Yaml;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class YamlLoader {
+
+    public Map<String, Object> load(String filePath) throws IOException {
+        try (InputStream is = Files.newInputStream(Path.of(filePath))) {
+            return new Yaml().load(is);
+        }
+    }
 }
